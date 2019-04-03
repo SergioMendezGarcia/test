acf36a8c52870ae8414b12dac8572291eadf99da
SergioMendezGarcia <a8alumno@aules.local>
2019-04-03 10:37:51 +0200

    actualizacion de ejercicios dia 2

--- Js1/main.js
+++ Js1/main.js
@@ -4,12 +4,28 @@ let sect = document.getElementById('sect');
 //     dihola('Sergio');
 // };
 
-const dihola = function(x) {
-    alert('Hola ' + x);
-};
+// const dihola = function(x) {
+//     alert('Hola ' + x);
+// }
 
-(function(x) {
-    sect.onclick = function() {
-        dihola(x);
+// (function(x) {
+//     sect.onclick = function() {
+//         dihola(x);
+//     }
+// })("Sergio");
+
+const mascotas = ()=>{
+    let lista = [];
+    for ( let i = 1; i <= 1000; i++ ) {
+        lista.push("Mascota" + i);
     }
-})("Sergio");
+
+    return(lista);
+}
+
+
+window.onload = function(){
+    for ( let i = 0; i<mascotas().length; i++) {
+        lmascotas.innerHTML += "<li>" + mascotas()[i] + "</li>";
+    }
+};
