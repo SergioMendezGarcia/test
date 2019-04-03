c4955ccbc9055371bbcc108028d4bcdf5e8c4572
SergioMendezGarcia <a8alumno@aules.local>
2019-04-03 14:01:23 +0200

    actualizacion de ejercicios

--- Js1/main.js
+++ Js1/main.js
@@ -16,8 +16,10 @@ let sect = document.getElementById('sect');
 (function() {
 const mascotas = ()=>{
     let lista = [];
+    let er = Math.floor(Math.random()*14+1);
+    let pr = Math.floor(Math.random()*200+50);
     for ( let i = 1; i <= 1000; i++ ) {
-        lista.push("Mascota" + i);
+        lista.push({id:i,nombre:'Mascota'+i,edad:er,precio:pr});
     }
 
     return lista;
@@ -26,7 +28,7 @@ const mascotas = ()=>{
 const imprimirMascotas = ()=>{
     let nmascotas = "";
     for ( let i = 0; i<mascotas().length; i++) {
-        nmascotas += "<li>" + mascotas()[i] + "</li>";
+        nmascotas += "<li>" + mascotas()[i].id + "- " + mascotas()[i].nombre + ", " + mascotas()[i].edad + " años, " + mascotas()[i].precio + "€</li>";
     }
     lmascotas.innerHTML = nmascotas;
 }
